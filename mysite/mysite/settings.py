ce5a1561fec0f45ac10c7e536fc4a184fccbc347
Vladimir <vlad1m1r.trapeza777@gmail.com>
2022-12-14 18:45:52 +0300

    Beta version

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-   'default': {
-      'ENGINE': 'django.db.backends.postgresql',
-      'NAME': 'postgres',
-      'USER': 'postgres',
-      'PASSWORD': '123456',
-      'HOST': 'db',
-      'PORT': '5432'
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#    'default': {
+#       'ENGINE': 'django.db.backends.postgresql',
+#       'NAME': 'postgres',
+#       'USER': 'postgres',
+#       'PASSWORD': '123456',
+#       'HOST': 'db',
+#       'PORT': '5432'
+#    }
+# }
 
 
 
